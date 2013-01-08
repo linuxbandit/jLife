c9fef9f962d2ec1864e2b90d1bb4db019df29093
grasshopper <bellicaf@tcd.ie>
2013-01-08 09:00:46 +0000

    WTF happened and what did I do i only wanted to learn reverting in git.. messy commit history wtf

--- src/fab/life/Life.java
+++ src/fab/life/Life.java
@@ -91,7 +91,7 @@ public class Life {
                 backbuffer[i][j] = neighbours(i, j); 
             }
         }        
-        lattice = backbuffer;
+        lattice = backbuffer; //?
     }
     
     private boolean neighbours(int x, int y)
@@ -102,7 +102,7 @@ public class Life {
         // x-1, y   x, y    x+1, y
         //          x, y+1
         
-        //Moore neighborhood (not used)
+        //Moore neighborhood
         
         // x-1, y-1  x, y-1    x+1, y-1
         // x-1, y    x, y      x+1, y
@@ -110,6 +110,7 @@ public class Life {
         
         
         //WORST FIRST SOLUTION EVER as for code elegance (laziness + short on time)
+        //TODO: edge of the matrix handling
         short num = 0;
         if(lattice[x][y-1])
         {
@@ -128,19 +129,19 @@ public class Life {
             num++;
         }
         
-        if(lattice[x][y-1])
+        if(lattice[x-1][y-1])
         {
             num++;
         }
-        if(lattice[x][y+1])
+        if(lattice[x-1][y+1])
         {
             num++;
         }
-        if(lattice[x-1][y])
+        if(lattice[x+1][y-1])
         {
             num++;
         }
-        if(lattice[x+1][y])
+        if(lattice[x+1][y+1])
         {
             num++;
         }
